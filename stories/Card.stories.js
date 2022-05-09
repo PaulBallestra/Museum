93893d5642baf5825da5ec0c80a2eff03931f358
PaulBallestra <paulballestra@yahoo.fr>
2022-05-09 18:30:47 +0200

    Card Abonnements Done :white_check_mark:

--- stories/Card.stories.js
+++ stories/Card.stories.js
@@ -11,7 +11,61 @@ export default {
 };
 
 const Template = ({ label, price }) => {
-    return `<div class="card-otherabonnement">
+
+    if(label == 'Basic'){
+        return `<div class="card-otherabonnement">
+                
+                <h1 class="card-title"> ${label} </h1>
+                
+                <p class="card-corp"> \t&#10003; Accès aux expositions </p>
+                
+                <p class="card-corp"> \t&#10003; Accès aux événements </p>
+                
+                <p class="card-corp-disabled"> \t&#10003; Accès aux ateliers </p>
+                
+                <p class="card-corp-disabled"> \t&#10003; Coupe-fil </p>
+                
+                <p class="card-corp-disabled"> \t&#10003; Accès aux collections privées </p>
+                
+                <div class="card-priceperyear">
+                    <h1 class="card-price"> ${price} </h1>
+                    <p class="card-corp" style="margin: auto 0;">/An</p>
+                </div>
+                
+                     
+                <div class="card-priceperyear">
+                    <button class="btn-card-others">Button</button>
+                </div>
+               
+            </div>`;
+    }else if(label == 'Standard'){
+        return `<div class="card-otherabonnement">
+                
+                <h1 class="card-title"> ${label} </h1>
+                
+                <p class="card-corp"> \t&#10003; Accès aux expositions </p>
+                
+                <p class="card-corp"> \t&#10003; Accès aux événements </p>
+                
+                <p class="card-corp"> \t&#10003; Accès aux ateliers </p>
+                
+                <p class="card-corp-disabled"> \t&#10003; Coupe-fil </p>
+                
+                <p class="card-corp-disabled"> \t&#10003; Accès aux collections privées </p>
+                
+                <div class="card-priceperyear">
+                    <h1 class="card-price"> ${price} </h1>
+                    <p class="card-corp" style="margin: auto 0;">/An</p>
+                </div>
+                
+                     
+                <div class="card-priceperyear">
+                    <button class="btn-card-others">Button</button>
+                </div>
+               
+            </div>`;
+    }else{
+        return `<div class="card-premiumabonnement">
                 
                 <h1 class="card-title"> ${label} </h1>
                 
@@ -30,9 +84,13 @@ const Template = ({ label, price }) => {
                     <p class="card-corp" style="margin: auto 0;">/An</p>
                 </div>
                 
-                <button>Button</button>
+                     
+                <div class="card-priceperyear">
+                    <button class="btn-card-premium">Button</button>
+                </div>
                
             </div>`;
+    }
 };
 
 //BasicAbonnement Card
@@ -49,3 +107,9 @@ StandardAbonnement.args = {
     price: '333€',
 };
 
+//PremiumAbonnement Card
+export const PremiumAbonnement = Template.bind({});
+PremiumAbonnement.args = {
+    label: 'Premium',
+    price: '433€',
+};
